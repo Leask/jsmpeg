a5738aa92efb9032228febbb2415d35df50d2103
Leask Huang <i@leaskh.com>
2015-07-25 22:47:03 +0800

    [22.7346199538444,114.225289110468] Frozen @ Sat Jul 25 22:47:03 HKT 2015

--- stream-server.js
+++ stream-server.js
@@ -9,7 +9,7 @@ if( process.argv.length < 3 ) {
 
 var STREAM_SECRET = process.argv[2],
 	STREAM_PORT = process.argv[3] || 18972,
-	WEBSOCKET_PORT = process.argv[4] || 8084,
+	WEBSOCKET_PORT = process.argv[4] || 18084,
 	STREAM_MAGIC_BYTES = 'jsmp'; // Must be 4 bytes
 
 var width = 320,
